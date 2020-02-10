b3521f02cb21be77e5bb60efd6fd7e61e870246c
DaliborMarkovic <cccp_100@hotmail.com>
2020-02-10 17:54:31 +0100

    1002

---
+++ mmain.js
@@ -0,0 +1,8 @@
+function reakcija() {
+    document.write("<h1>Reakcija</h1>");
+}
+
+const novaReakcija = ()=>{
+    document.write(`<h1>Reakcija koja sve cisti i ubacuje sliku</h1><img src='../../slike/tesla.jpg'>`);
+
+}
