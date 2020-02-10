bcd246a02bc96a1617c0fa8f7a63f00d07d9a049
DaliborMarkovic <cccp_100@hotmail.com>
2020-02-10 20:08:47 +0100

    Delete mmain.js

--- mmain.js
+++
@@ -1,8 +0,0 @@
-function reakcija() {
-    document.write("<h1>Reakcija</h1>");
-}
-
-const novaReakcija = ()=>{
-    document.write(`<h1>Reakcija koja sve cisti i ubacuje sliku</h1><img src='../../slike/tesla.jpg'>`);
-
-}
