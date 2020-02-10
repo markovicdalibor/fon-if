8e60cf1e14afa3092b9fbbbe22b78440c4aa421f
DaliborMarkovic <cccp_100@hotmail.com>
2020-02-10 18:15:44 +0100

    novo

---
+++ 1002/mmain.js
@@ -0,0 +1,20 @@
+/*function reakcija() {
+    document.write("<h1>Reakcija</h1>");
+}
+
+const novaReakcija = ()=>{
+    document.write(`<h1>Reakcija koja sve cisti i ubacuje sliku</h1><img src='../../slike/tesla.jpg'>`);
+
+}*/
+
+/*let user = prompt('unesi ime');
+let pass= prompt('unesi password');
+const reakcijaClick =()=>{
+    if (user=="admin" && pass=="admin") {
+        document.write(`Zdravo ${user}<br>Pristup odobrem`)
+    }
+}*/
+
+function prikaziSliku() {
+    document.write(`<h1>Nova slika</h1><img src='../../slike/tesla.jpg'>`);
+}
